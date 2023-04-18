83052506f3b91459423881b1c5f18786776f9f76
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 17:30:45 +0500

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,73 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        if (cathetusLength == 0) {
+            System.out.print("");
+        } else if (cathetusLength == 1) {
+            System.out.println("*");
+        } else if (cathetusLength == 2) {
+            System.out.println(" *");
+            System.out.println("**");
+        } else if (cathetusLength == 3) {
+            System.out.println("  *");
+            System.out.println(" **");
+            System.out.println("***");
+        } else if (cathetusLength == 4) {
+            System.out.println("   *");
+            System.out.println("  **");
+            System.out.println(" ***");
+            System.out.println("****");
+        } else if (cathetusLength == 5) {
+            System.out.println("    *");
+            System.out.println("   **");
+            System.out.println("  ***");
+            System.out.println(" ****");
+            System.out.println("*****");
+        } else if (cathetusLength == 6) {
+            System.out.println("     *");
+            System.out.println("    **");
+            System.out.println("   ***");
+            System.out.println("  ****");
+            System.out.println(" *****");
+            System.out.println("******");
+        } else if (cathetusLength == 7) {
+            System.out.println("      *");
+            System.out.println("     **");
+            System.out.println("    ***");
+            System.out.println("   ****");
+            System.out.println("  *****");
+            System.out.println(" ******");
+            System.out.println("*******");
+        } else if (cathetusLength == 8) {
+            System.out.println("       *");
+            System.out.println("      **");
+            System.out.println("     ***");
+            System.out.println("    ****");
+            System.out.println("   *****");
+            System.out.println("  ******");
+            System.out.println(" *******");
+            System.out.println("********");
+        } else if (cathetusLength == 9) {
+            System.out.println("        *");
+            System.out.println("       **");
+            System.out.println("      ***");
+            System.out.println("     ****");
+            System.out.println("    *****");
+            System.out.println("   ******");
+            System.out.println("  *******");
+            System.out.println(" ********");
+            System.out.println("*********");
+        } else if (cathetusLength == 10) {
+            System.out.println("        *");
+            System.out.println("       **");
+            System.out.println("      ***");
+            System.out.println("     ****");
+            System.out.println("    *****");
+            System.out.println("   ******");
+            System.out.println("  *******");
+            System.out.println(" ********");
+            System.out.println("*********");
+       }
 
     }
 
